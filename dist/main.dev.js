fc2fb1d33db3ec11d459260e1e75fe1b8f5f74ea
Pandademic <stewie.gupte@gmail.com>
2021-08-16 13:21:37 -0400

    v3.0or4.0

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -1,5 +1,30 @@
 "use strict";
 
-function vanish() {
-  document.getElementsByClassName("lds-ellipsis").style.display = none;
+function preload() {}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Jrp6lCuIC/model.json', modelLoaded);
+}
+
+function draw() {
+  image(video, 0, 0, 200, 200);
+  classifier.classify(video, gotResult);
+}
+
+function modelLoaded() {
+  console.log("Model loaded!");
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById('result_object_name').innerHTML = results[0].label;
+    document.getElementById('result_object_accuracy').innerHTML = results[0].confidence.toFixed(3);
+  }
 }
